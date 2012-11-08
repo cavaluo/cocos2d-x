22db4baf47794df1c0cc943414d30c33ade7a68a
James Chen <jianhua.chen@cocos2d-x.org>
2012-11-08 15:50:28 +0800

    Updated Copyright for CCArray.h

--- cocos2dx/cocoa/CCArray.h
+++ cocos2dx/cocoa/CCArray.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 Copyright (c) 2010 ForzeField Studios S.L. http://forzefield.com
-Copyright (c) 2010 cocos2d-x.org
+Copyright (c) 2010 - 2012 cocos2d-x.org
 
 http://www.cocos2d-x.org
 
